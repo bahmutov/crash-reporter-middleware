9af63c9a616cbc2939aa3a53dace2b12f0b33115
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-10-05 18:07:19 -0400

    refactoring

--- index.js
+++ index.js
@@ -9,35 +9,49 @@ var renderVars = require('render-vars');
 la(check.fn(renderVars), 'expected render vars function', renderVars);
 
 var pkg = require('./package.json');
+var excludedDomains = ['localhost', '127.0.0.1'];
 
-function initRaygunClient(config, addRenderValue, commitId) {
+function noop() {}
+
+function initRaygunClient(raygunApiKey, addRenderValue, commitId) {
   la(check.fn(addRenderValue), 'missing addRenderValue function');
   la(check.unemptyString(commitId), 'unknown commit id', commitId);
   la(check.shortCommitId(commitId), 'not a commit sha', commitId);
 
-  var raygunApiKey = config('RAYGUN');
-  var raygunClient;
-  if (check.unemptyString(raygunApiKey)) {
-    raygunClient = new raygun.Client().init({ apiKey: raygunApiKey });
-    process.on('uncaughtException', function onError(err) {
-      raygunClient.send(err, { commit: commitId });
-      console.error(err);
-      process.exit(-1);
-    });
+  var raygunClient = new raygun.Client().init({ apiKey: raygunApiKey });
+  process.on('uncaughtException', function onError(err) {
+    raygunClient.send(err, { commit: commitId });
+    console.error(err);
+    process.exit(-1);
+  });
 
-    // raygun requires version to be 4 numbers, add fake one
-    var raygunVersion = '0.' + pkg.version;
-    raygunClient.setVersion(raygunVersion);
+  // raygun requires version to be 4 numbers, add fake one
+  var raygunVersion = '0.' + pkg.version;
+  raygunClient.setVersion(raygunVersion);
 
-    addRenderValue('raygunApiKey', raygunApiKey || '');
-    addRenderValue('raygunVersion', raygunVersion || '');
-    addRenderValue('raygunExcludedHostnames', JSON.stringify(['localhost', '127.0.0.1']));
-    console.log('initialized error reporting, commit id %s, version %s',
-      commitId, raygunVersion);
-  } else {
-    console.log('skipping error reporting setup - missing api key');
-  }
+  addRenderValue('raygunApiKey', raygunApiKey || '');
+  addRenderValue('raygunVersion', raygunVersion || '');
+  addRenderValue('raygunExcludedHostnames', JSON.stringify(excludedDomains));
+
+  console.log('initialized Raygun error reporting, commit id %s, version %s',
+    commitId, raygunVersion);
   return raygunClient;
+}
+
+function initMiddleware(config, addRenderValue, commitId) {
+  la(check.fn(addRenderValue), 'missing addRenderValue function');
+  la(check.unemptyString(commitId), 'unknown commit id', commitId);
+  la(check.shortCommitId(commitId), 'not a commit sha', commitId);
+
+  console.log('init crash middleware for commit %s', commitId);
+
+  var raygunApiKey = config('RAYGUN');
+  if (check.unemptyString(raygunApiKey)) {
+    return initRaygunClient(raygunApiKey, addRenderValue, commitId);
+  }
+
+  console.log('skipping error reporting setup - missing api key');
+  return noop;
 }
 
 function initCrashReporting(config, server) {
@@ -45,7 +59,7 @@ function initCrashReporting(config, server) {
   la(check.fn(server), 'missing server', server);
 
   var addRenderValue = renderVars.bind(null, server);
-  var hookReporter = initRaygunClient.bind(null, config, addRenderValue);
+  var hookReporter = initMiddleware.bind(null, config, addRenderValue);
 
   return getLastCommitId()
     .then(hookReporter);
@@ -55,5 +69,9 @@ module.exports = initCrashReporting;
 
 if (!module.exports) {
   // demo use case
-
+  initCrashReporting(noop)
+    .then(function (client) {
+      // there should be no client
+      console.log('middleware client is noop?', client === noop);
+    }).done();
 }
