a1cb99ab4dcb0107554f4adbd1da88a30d45686d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-01 11:19:28 -0500

    feat(deps): updated some dependencies, using standard for linting

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-require('lazy-ass');
-var check = require('check-more-types');
+const la = require('lazy-ass')
+const check = require('check-more-types')
 
 // available crash reporting clients
-var initRaygunClient = require('./src/raygun');
+var initRaygunClient = require('./src/raygun')
 
-var getLastCommitId = require('last-commit');
-la(check.fn(getLastCommitId), 'missing last commit function');
-var renderVars = require('render-vars');
-la(check.fn(renderVars), 'expected render vars function', renderVars);
+var getLastCommitId = require('last-commit')
+la(check.fn(getLastCommitId), 'missing last commit function')
+var renderVars = require('render-vars')
+la(check.fn(renderVars), 'expected render vars function', renderVars)
 
-function noop() {}
+function noop () {}
 
-function isUnknown(id) {
-  return id === 'unknown';
+function isUnknown (id) {
+  return id === 'unknown'
 }
 
-function isValidCommit(id) {
+function isValidCommit (id) {
   return check.shortCommitId(id) || isUnknown(id)
 }
 
-function getRaygunApiKey(config) {
-  return config('RAYGUN') || config('RAYGUN_APIKEY');
+function getRaygunApiKey (config) {
+  return config('RAYGUN') || config('RAYGUN_APIKEY')
 }
 
-function initMiddleware(config, addRenderValue, commitId) {
-  la(check.fn(addRenderValue), 'missing addRenderValue function');
-  la(check.unemptyString(commitId), 'unknown commit id', commitId);
-  la(isValidCommit(commitId), 'not a commit sha', commitId);
+function initMiddleware (config, addRenderValue, commitId) {
+  la(check.fn(addRenderValue), 'missing addRenderValue function')
+  la(check.unemptyString(commitId), 'unknown commit id', commitId)
+  la(isValidCommit(commitId), 'not a commit sha', commitId)
 
-  console.log('init crash middleware for commit "%s"', commitId);
+  console.log('init crash middleware for commit "%s"', commitId)
 
-  var raygunApiKey = getRaygunApiKey(config);
+  var raygunApiKey = getRaygunApiKey(config)
   if (check.unemptyString(raygunApiKey)) {
-    return initRaygunClient(raygunApiKey, addRenderValue, commitId);
+    return initRaygunClient(raygunApiKey, addRenderValue, commitId)
   }
 
-  console.log('skipping error reporting setup - missing api key');
-  return noop;
+  console.log('skipping error reporting setup - missing api key')
+  return noop
 }
 
-function initCrashReporting(config, server) {
-  la(check.fn(config), 'missing config get function', config);
-  la(check.fn(server), 'missing server', server);
+function initCrashReporting (config, server) {
+  la(check.fn(config), 'missing config get function', config)
+  la(check.fn(server), 'missing server', server)
 
-  var addRenderValue = renderVars.bind(null, server);
-  var hookReporter = initMiddleware.bind(null, config, addRenderValue);
+  var addRenderValue = renderVars.bind(null, server)
+  var hookReporter = initMiddleware.bind(null, config, addRenderValue)
 
   return getLastCommitId()
-    .then(hookReporter);
+    .then(hookReporter)
 }
 
-module.exports = initCrashReporting;
+module.exports = initCrashReporting
 
 if (!module.parent) {
-  console.log('demo use case');
+  console.log('demo use case')
   initCrashReporting(noop, noop)
     .then(function (client) {
       // there should be no client
-      console.log('middleware client is noop?', client === noop);
-      la(client === noop, 'not noop', client);
-    }).done();
+      console.log('middleware client is noop?', client === noop)
+      la(client === noop, 'not noop', client)
+    }).done()
 }
--- src/raygun.js
+++ src/raygun.js
@@ -1,48 +1,48 @@
-require('lazy-ass');
-var check = require('check-more-types');
-var raygun = require('raygun');
-var exists = require('fs').existsSync;
-var read = require('fs').readFileSync;
+const la = require('lazy-ass')
+var check = require('check-more-types')
+var raygun = require('raygun')
+var exists = require('fs').existsSync
+var read = require('fs').readFileSync
 
-var excludedDomains = ['localhost', '127.0.0.1'];
+var excludedDomains = ['localhost', '127.0.0.1']
 
-function noop() {}
+function noop () {}
 
-function initRaygunClient(raygunApiKey, addRenderValue, commitId) {
-  la(check.unemptyString(raygunApiKey), 'missing raygun api key', raygunApiKey);
-  la(check.fn(addRenderValue), 'missing addRenderValue function');
-  la(check.unemptyString(commitId), 'unknown commit id', commitId);
+function initRaygunClient (raygunApiKey, addRenderValue, commitId) {
+  la(check.unemptyString(raygunApiKey), 'missing raygun api key', raygunApiKey)
+  la(check.fn(addRenderValue), 'missing addRenderValue function')
+  la(check.unemptyString(commitId), 'unknown commit id', commitId)
 
-  var raygunVersion;
-  var raygunClient = new raygun.Client().init({ apiKey: raygunApiKey });
+  var raygunVersion
+  var raygunClient = new raygun.Client().init({ apiKey: raygunApiKey })
 
-  function onError(err) {
-    raygunClient.send(err, { commit: commitId });
-    console.error(err);
-    process.exit(-1);
+  function onError (err) {
+    raygunClient.send(err, { commit: commitId })
+    console.error(err)
+    process.exit(-1)
   }
 
-  process.on('uncaughtException', onError);
+  process.on('uncaughtException', onError)
 
   if (exists('package.json')) {
-    var pkg = JSON.parse(read('./package.json'));
+    var pkg = JSON.parse(read('./package.json'))
     // raygun requires version to be 4 numbers, add fake one
-    raygunVersion = '0.' + pkg.version;
-    raygunClient.setVersion(raygunVersion);
+    raygunVersion = '0.' + pkg.version
+    raygunClient.setVersion(raygunVersion)
   }
 
-  addRenderValue('raygunApiKey', raygunApiKey || '');
-  addRenderValue('raygunVersion', raygunVersion || '');
-  addRenderValue('raygunExcludedHostnames', JSON.stringify(excludedDomains));
+  addRenderValue('raygunApiKey', raygunApiKey || '')
+  addRenderValue('raygunVersion', raygunVersion || '')
+  addRenderValue('raygunExcludedHostnames', JSON.stringify(excludedDomains))
 
   console.log('initialized Raygun error reporting, commit id %s, version %s',
-    commitId, raygunVersion);
-  return raygunClient.expressHandler;
+    commitId, raygunVersion)
+  return raygunClient.expressHandler
 }
 
-module.exports = initRaygunClient;
+module.exports = initRaygunClient
 
 if (!module.parent) {
-  console.log('demo raygun case');
-  initRaygunClient('apiKey', noop, 'unknown');
+  console.log('demo raygun case')
+  initRaygunClient('apiKey', noop, 'unknown')
 }
